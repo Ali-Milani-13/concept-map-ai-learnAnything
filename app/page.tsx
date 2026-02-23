37e7be238945355bb75a2f173ff117fc5b520534
Ali Milani <haythamtemplar@hotmail.com>
2026-02-23 14:41:40 +0330

    [UPDATE] preparing for FSD design

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import MapCanvas from '@/components/MapCanvas';
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col">
-      <MapCanvas />
-    </main>
-  );
-}
